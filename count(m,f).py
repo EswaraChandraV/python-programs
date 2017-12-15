21a4f14d393048ee324a05e86f73437878f309e7
eswarachandrav <34371641+EswaraChandraV@users.noreply.github.com>
2017-12-15 14:39:59 +0530

    count(m,f)

---
+++ count(m,f).py
@@ -0,0 +1,18 @@
+s=input()
+d={}
+list1=[]
+list2=[]
+f_count=0
+m_count=0
+for i in range(len(s)):
+  if(s[i]=="F"):
+    list1.append(s[i])
+    f_count+=1
+  else:
+    list2.append(s[i])
+    m_count+=1
+print("m_count:",m_count)
+print("f_count:",f_count)
+print("".join(list1),end="")
+print("".join(list2))
+  
